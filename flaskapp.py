9234b55d7fc9e6df6c527a3a0f0782cbe1c4d729
Kacper <random8902@gmail.com>
2016-03-18 23:03:11 +0100

    Done is better than perfect

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from test import Query
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
 
@@ -8,10 +9,15 @@ app = Flask(__name__)
 def index():
     return "Sambastic Alle"
 
-@app.route("/mvp")
-def mvp():
+@app.route("/mvp/<req>")
+def mvp(req):
     q = Query()
-    offers = q.query("thinkpad x230")["offers"]
+    offers = q.query(req)["offers"]
+    prices = []
+    for offer in offers:
+        prices.append(offer['prices']['buyNow'])
+    plt.hist(prices)
+    plt.savefig('static/hist.png')
     return render_template("mvp.html", offers=offers)
 
 if __name__ == '__main__':

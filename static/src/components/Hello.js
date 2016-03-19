7cf884ad926aa7b1bb61726ba777a1677f6c8f00
Filip Borowiec <filip@samba.tv>
2016-03-19 13:51:11 +0100

    s

--- static/src/components/Hello.js
+++ static/src/components/Hello.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button, Card, Row, Col } from 'react-materialize';
+import TrackerCard from './TrackerCard';
+import { Button, Card, Col, Footer, Navbar, NavItem, Row } from 'react-materialize';
 
 export default class Hello extends React.Component {
     constructor(...args) {
@@ -10,19 +11,45 @@ export default class Hello extends React.Component {
 
     getContent() {
         return (
-            <Card>
-                Hej
-            </Card>
+            <div>
+                <TrackerCard id={0} />
+                <TrackerCard id={1} />
+            </div>
         )
     }
 
     render() {
         return (
-            <Row>
-                <Col s={12} className="">
-                    {this.getContent()}
-                </Col>
-            </Row>
+            <div>
+                <Navbar brand="Sambastic" right>
+                    <NavItem href='#'>Refresh</NavItem>
+                </Navbar>
+                <div className="container">
+                    <Row>
+                        <Col s={6} className="">
+                            <TrackerCard id={0} />
+                        </Col>
+                        <Col s={6} className="">
+                            <TrackerCard id={1} />
+                        </Col>
+                    </Row>
+                </div>
+                <Footer copyrights="&copy; 2016 Sambastic"
+                        moreLinks={
+                            <a className="grey-text text-lighten-4 right" href="#!">See our blog</a>
+                        }
+                        links={
+                            <ul>
+                              <li><a className="grey-text text-lighten-3" href="//allegro.pl">Sponsor</a></li>
+                              <li><a className="grey-text text-lighten-3" href="//allegro.tech/braincode/">Braincode 2016</a></li>
+                              <li><a className="grey-text text-lighten-3" href="//allegro.pl">allegro.pl</a></li>
+                            </ul>
+                        }
+                        className='example'>
+                    <h5 className="white-text">Pricemize</h5>
+                    <p className="grey-text text-lighten-4">Find best deals without effort</p>
+                </Footer>
+            </div>
         );
     }
 }
--- static/src/components/TrackerCard.js
+++ static/src/components/TrackerCard.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Button, Card, Row, Col } from 'react-materialize';
+import { Button, Card, CardTitle, Row, Col } from 'react-materialize';
+
+let getTrackerForId = id => {
+    switch (id) {
+        case 0:
+            return {
+                name: 'iMac',
+                min_price: 5200,
+                max_price: 7200,
+                query_string: 'iMac display',
+                img_src: '//mac-technical-support.com/wp-content/uploads/2015/12/imac-error-support.jpg'
+            };
+        case 1:
+            return {
+                name: 'MacBook',
+                min_price: 1600,
+                max_price: 2100,
+                query_string: 'MacBookPro 12\'',
+                img_src: '//tctechcrunch2011.files.wordpress.com/2015/04/macbook-front.jpg?w=1279&h=727&crop=1'
+            }
+    }
+};
 
 export default class TrackerCard extends React.Component {
     constructor(...args) {
         super(...args);
-
-        this.getContent = this.getContent.bind(this);
     }
 
-    getContent() {
+    render() {
+        let tracker = getTrackerForId(this.props.id);
         return (
-            <Card>
-                Hej
+            <Card header={<CardTitle image={tracker.img_src}>{tracker.name}</CardTitle>}>
+                {tracker.query_string}
             </Card>
         )
     }
